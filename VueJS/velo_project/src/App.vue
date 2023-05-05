e1de9ebba53b1deb6f96afa82cd1ad52268e89c8
Florian <escande.florian.chatco@gmail.com>
2023-05-05 11:32:36 +0200

    create-new-vueproject

--- VueJS/velo_project/src/App.vue
+++ VueJS/velo_project/src/App.vue
@@ -5,20 +5,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-      <ul>
-          <li>
-              <a href="">Accueil</a>
-          </li>
-          <li>
-              <a href="">Accueil</a>
-          </li>
-          <li>
-              <a href="">Accueil</a>
-          </li>
-          <li>
-              <a href="">Accueil</a>
-          </li>
-      </ul>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
   </header>
 
   <RouterView />
