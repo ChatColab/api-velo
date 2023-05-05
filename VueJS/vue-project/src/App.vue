a9c4edd28456c9c580c2564315c657dd62b2d742
Florian <escande.florian.chatco@gmail.com>
2023-05-05 11:10:16 +0200

    script-import-php-fonctionne

--- VueJS/vue-project/src/App.vue
+++ VueJS/vue-project/src/App.vue
@@ -5,16 +5,20 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <ul>
+          <li>
+              <a href="">Accueil</a>
+          </li>
+          <li>
+              <a href="">Accueil</a>
+          </li>
+          <li>
+              <a href="">Accueil</a>
+          </li>
+          <li>
+              <a href="">Accueil</a>
+          </li>
+      </ul>
   </header>
 
   <RouterView />
