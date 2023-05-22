886a0b97e5feaf9fd438ff90f181afd9f9b43e1c
Florian <escande.florian.chatco@gmail.com>
2023-05-22 11:01:26 +0200

    filtrage+pagination-wip

--- VueJS/velo_project/src/components/Data.vue
+++ VueJS/velo_project/src/components/Data.vue
@@ -1,43 +1,43 @@
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
     methods: {
-        fetchData() {
-            const token = '65317c8ad9a5eb26883845c7d842a30f3aca84ec62d0c7b814247f52308f010995afe46ecf7c70733c2ae4c3c6770cd30b228bc8340baa75c89fe3b7080d73da221b4efc2fe7c87aa7f92d8f50a12862c601456b6bd7ad6701d0de1f573abd6d80572a5ba69d661bb8dc01790253c9e92e46f27b71f02820d877dc4432d3a92d';
-            const latitude = 49.8652904994832;
-            const longitude = 2.3786318;
-            const range = 0.5;
+        donneMoiToutesTesDonneesHAHAHAHAH() {
+            const token = '65317c8ad9a5eb26883845c7d842a30f3aca84ec62d0c7b814247f52308f010995afe46ecf7c70733c2ae4c3c6770cd30b228bc8340baa75c89fe3b7080d73da221b4efc2fe7c87aa7f92d8f50a12862c601456b6bd7ad6701d0de1f573abd6d80572a5ba69d661bb8dc01790253c9e92e46f27b71f02820d877dc4432d3a92d'
+            const latitude = 49.8652904994832
+            const longitude = 2.3786318
+            const range = 0.2
 
-            const minLatitude = latitude - range;
-            const maxLatitude = latitude + range;
-            const minLongitude = longitude - range;
-            const maxLongitude = longitude + range;
+            const minLatitude = latitude - range
+            const maxLatitude = latitude + range
+            const minLongitude = longitude - range
+            const maxLongitude = longitude + range
 
-            const apiUrl = `http://localhost:1337/api/velos/?x_gte=${minLongitude}&x_lte=${maxLongitude}&y_gte=${minLatitude}&y_lte=${maxLatitude}`;
+            const apiUrl = `http://localhost:1337/api/velos?filters[x][$gte]=${minLongitude}&filters[x][$lte]=${maxLongitude}&filters[y][$gte]=${minLatitude}&filters[y][$lte]=${maxLatitude}`
 
             const config = {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
-            };
+            }
 
             axios.get(apiUrl, config)
                 .then(response => {
-                    const data = response.data.data;
-                    console.log(data);
+                    const data = response.data.data
+                    console.log(data)
                 })
                 .catch(error => {
-                    console.error(error);
-                });
+                    console.error(error)
+                })
         },
     },
-};
+}
 </script>
 
 <template>
     <div>
         <p>guette le ctrl maj i</p>
-        <button @click="fetchData">Afficher les résultats</button>
+        <button @click="donneMoiToutesTesDonneesHAHAHAHAH">Afficher les résultats</button>
     </div>
 </template>
