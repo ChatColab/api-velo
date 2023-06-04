8b06f46fd064471b8ee74893b4662603a4fbd0a4
Florian <escande.florian.chatco@gmail.com>
2023-06-04 23:43:28 +0200

    gpt-markers

--- VueJS/velo_project/src/components/ComposantGeo.vue
+++ VueJS/velo_project/src/components/ComposantGeo.vue
@@ -2,39 +2,79 @@
 import L from 'leaflet'
 
 export default {
+    props: ['parks'],
+    data() {
+        return {
+            map: null,
+            currentCoordinates: [49.8652904994832, 2.3786318],
+            defaultZoom: 12,
+            // Add this property to store markers
+            markers: [],
+        }
+    },
     mounted() {
         this.initMap()
     },
     methods: {
         initMap() {
-            const defaultCoordinates = [49.8652904994832, 2.3786318]
-            const defaultZoom = 12
             const mapElement = document.getElementById('map')
-            // var map = L.map(mapElement, {
-            //     center: defaultCoordinates,
-            //     zoom: defaultZoom
-            // })
-            const map = L.map(mapElement).setView(defaultCoordinates, defaultZoom)
+            this.map = L.map(mapElement).setView(this.currentCoordinates, this.defaultZoom)
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-            }).addTo(map)
+            }).addTo(this.map)
+
+            // Listen for moveend event
+            this.map.on('moveend', this.fetchAndDisplayMarkers)
+        },
+        updateMapCenter() {
+            this.map.setView(this.currentCoordinates, this.defaultZoom)
+        },
+        fetchAndDisplayMarkers() {
+            this.$emit('updateMarkers', this.map.getBounds())
+        },
+        displayMarkers(parks) {
+            // Remove old markers
+            this.markers.forEach(marker => this.map.removeLayer(marker))
+
+            // Add new markers
+            parks.forEach(park => {
+                let color = 'green';
+
+                if (park.attributes.gratuit) {
+                    color = 'red';
+                }
+
+                let markerIcon = L.icon({
+                    iconUrl: `http://leafletjs.com/examples/custom-icons/leaf-${color}.png`,
+                    shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+
+                    iconSize:     [38, 95],
+                    shadowSize:   [50, 64],
+                    iconAnchor:   [22, 94],
+                    shadowAnchor: [4, 62],
+                    popupAnchor:  [-3, -76]
+                })
+
+                let marker = L.marker([park.attributes.y, park.attributes.x], {icon: markerIcon}).addTo(this.map)
+                // Add a popup with the park's name and available spaces
+                marker.bindPopup(`<b>test</b><br>${park.attributes.capacite} places disponibles.`)
+                this.markers.push(marker)
+            })
         },
     },
+    watch: {
+        currentCoordinates() {
+            this.updateMapCenter()
+        },
+        parks(newParks) {
+            this.displayMarkers(newParks)
+        },
+    },
+
 }
 </script>
 
 <template>
-  <div id="map"></div>
+    <div id="map" style="height: 500px; width: 100%;"></div>
 </template>
-
-<style scoped>
-#map{
-    height: 100vh;
-    width: 100%;
-    position: fixed;
-    top:0;
-    left:0;
-    z-index: 100;
-}
-</style>
--- VueJS/velo_project/src/views/AccueilView.vue
+++ VueJS/velo_project/src/views/AccueilView.vue
@@ -1,9 +1,30 @@
 <script setup>
-import AccueilTest from '../components/AccueilTest.vue'
+import { ref, onMounted } from 'vue'
+import ComposantGeo from "@/components/ComposantGeo.vue"
+import axios from 'axios'
+
+const currentCoordinates = ref([49.8652904994832, 2.3786318])
+const parks = ref([])
+
+async function fetchAndDisplayMarkers(bounds) {
+    const token = '65317c8ad9a5eb26883845c7d842a30f3aca84ec62d0c7b814247f52308f010995afe46ecf7c70733c2ae4c3c6770cd30b228bc8340baa75c89fe3b7080d73da221b4efc2fe7c87aa7f92d8f50a12862c601456b6bd7ad6701d0de1f573abd6d80572a5ba69d661bb8dc01790253c9e92e46f27b71f02820d877dc4432d3a92d'
+
+    const minLatitude = bounds.getSouthWest().lat
+    const maxLatitude = bounds.getNorthEast().lat
+    const minLongitude = bounds.getSouthWest().lng
+    const maxLongitude = bounds.getNorthEast().lng
+
+    const apiUrl = `http://localhost:1337/api/velos?filters[x][$gte]=${minLongitude}&filters[x][$lte]=${maxLongitude}&filters[y][$gte]=${minLatitude}&filters[y][$lte]=${maxLatitude}`
+
+    const response = await axios.get(apiUrl, { headers: { 'Authorization': `Bearer ${token}` } })
+    console.log(response.data)
+    // parks.value = response.data
+    parks.value = response.data.data
+}
 </script>
 
 <template>
-    <main>
-        <AccueilTest />
-    </main>
+    <div>
+        <ComposantGeo :parks="parks" @updateMarkers="fetchAndDisplayMarkers"/>
+    </div>
 </template>
